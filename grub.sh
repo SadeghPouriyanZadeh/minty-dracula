7828e81eb9c4140fcbba5e33a94f202f23684f98
SadeghPouriyanZadeh <pouriyan.sadegh@gmail.com>
2022-09-17 08:49:09 +0430

    feat: create grub.sh

---
+++ grub.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+apt install git
+git clone https://github.com/dracula/grub.git
+cd grub
+cp -r dracula/ /boot/grub/themes/dracula/
+sudo chmod 777 /etc/default/grub
+echo -e '\nGRUB_THEME=/boot/grub/themes/dracula/theme.txt' >> /etc/default/grub
+grub-mkconfig -o /boot/grub/grub.cfg
+cd ..
+rm -rf grub
