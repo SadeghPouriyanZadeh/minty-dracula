eb55525f0062a42849c6cd50d7d1aac97d1d9850
SadeghPouriyanZadeh <pouriyan.sadegh@gmail.com>
2022-09-19 01:54:12 +0430

    update grub.sh
    change permission from 777 to default 644

--- grub.sh
+++ grub.sh
@@ -4,7 +4,7 @@ apt install git
 git clone https://github.com/dracula/grub.git
 cd grub
 cp -r dracula/ /boot/grub/themes/dracula/
-sudo chmod 777 /etc/default/grub
+sudo chmod 644 /etc/default/grub
 echo -e '\nGRUB_THEME=/boot/grub/themes/dracula/theme.txt' >> /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
 cd ..
